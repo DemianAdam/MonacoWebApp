d0f138ca72b148b848bdcaba0d8ca00998f8e883
Demian <61812941+DemianAdam@users.noreply.github.com>
2025-03-20 18:38:17 -0300

    Configure Vite for deployment with base path and add deployment scripts to package.json

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/MonacoDev',
   plugins: [tailwindcss(), react()],
   server:{
     host: true,
